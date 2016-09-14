b006144a1a1c2877d4640d7c37a1d99742b495a9
Raycent <raycent.tan@flatironschool.com>
2016-09-14 15:40:12 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
 'use-strict';
+
+function h1Selector() {
+  return $('h1')
+}
+
+function liInOlSelector() {
+  return $('ol li')
+}
+
+function linkSelector() {
+  return $('#box4.box5 a')
+}
+
+function checkboxInputSelector() {
+  return $(:checkbox)
+}
