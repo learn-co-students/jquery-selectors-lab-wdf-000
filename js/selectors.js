d624b648a79230ea5518340ed13a7a500775e3b0
Raycent <raycent.tan@flatironschool.com>
2016-09-14 15:41:53 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -12,6 +12,10 @@ function linkSelector() {
   return $('#box4.box5 a')
 }
 
+function imageSelector() {
+  return $('img[alt="cat sleeping"]')
+}
+
 function checkboxInputSelector() {
-  return $(:checkbox)
+  return $(':checkbox')
 }
