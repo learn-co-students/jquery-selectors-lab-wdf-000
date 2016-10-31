cd644b3bed59ecba63ff9f4577a5576ff20dfbee
Bibi Khadiza <limakhadiza@yahoo.com>
2016-10-31 16:33:43 -0400

    done

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
 'use-strict';
+//
+$(document).ready(function(){
+  h1Selector();
+  liInOlSelector();
+  linkSelector();
+  imageSelector();
+  checkboxInputSelector();
+});
+
+function h1Selector(){
+  return $('h1');
+}
+
+function liInOlSelector(){
+  return $('ol li')
+}
+
+function linkSelector(){
+  return $('#box4 a')
+}
+
+function imageSelector(){
+  return $("img[alt='cat sleeping']")
+}
+
+function checkboxInputSelector(){
+  return $("input[type='checkbox']")
+}
