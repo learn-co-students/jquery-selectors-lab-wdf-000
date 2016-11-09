1bab8ca63f7f6dd11885335254cc4529bdb2d2db
Flatiron School <github email address>
2016-11-09 16:11:35 -0500

    done

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
 'use-strict';
+
+
+
+function h1Selector() {
+  return $("h1")
+}
+
+
+function liInOlSelector() {
+  return $("ol li")
+}
+
+
+function linkSelector() {
+  return $("#box4 a")
+}
+
+// $(".box5#box4 a") or this ^^^
+
+
+function imageSelector() {
+  return $("img[alt='cat sleeping']")
+}
+
+
+function checkboxInputSelector() {
+  return $(":checkbox")
+}
